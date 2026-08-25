04854258d5d01b32e3928fba4a72d5409516f16a
PC90-MI <p.c.90@hotmail.it>
2026-08-25 23:23:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vocabolario-v13";
+const CACHE_NAME = "lexico-v14";
 const APP_SHELL = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
