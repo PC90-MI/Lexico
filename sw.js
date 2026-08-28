0b6c131075173a016c1eccb05fd9446f95751277
PC90-MI <p.c.90@hotmail.it>
2026-08-28 14:48:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lexico-v14";
+const CACHE_NAME = "lexico-v15";
 const APP_SHELL = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
